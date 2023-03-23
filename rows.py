60f50425a35390e35336bc3a546b09f761c8c90b
Vaish01 <113707167+Vaish-cse@users.noreply.github.com>
2023-03-23 20:04:49 +0530

    Update rows.py

--- rows.py
+++ rows.py
@@ -1,7 +1,20 @@
-num_rows = int(input("Enter the number of rows: "))
+def sort_rows(lst):
 
-for i in range(1,num_rows+1):
-    if i % 2 == 0:
-        print(" "*(num_rows-i) + "# "*i)
-    else:
-        print(" "*(num_rows-i) + "* "*i)
+    return sorted(lst, key=lambda row: sum(row))
+
+def print_list(lst):
+
+    for row in lst:
+        print(" ".join(str(x) for x in row))
+
+
+rows = int(input("Enter the number of rows: "))
+cols = int(input("Enter the number of columns: "))
+
+lst = []
+for i in range(rows):
+    row = input("Enter the values for row {}: ".format(i+1))
+    lst.append([int(x) for x in row.split()])
+
+lst_sorted = sort_rows(lst)
+print_list(lst_sorted)
